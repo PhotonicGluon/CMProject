c3905094311d4023e0b5c80b704e89fec739a27b
Kan Onn Kit <kan_onn_kit@s2017.sst.edu.sg>
2018-08-29 09:19:00 +0800

    Update

--- CMProject/Views/Map/ViewController_ExtraDetail.swift
+++ CMProject/Views/Map/ViewController_ExtraDetail.swift
@@ -5,6 +5,9 @@
 //  Created by Kan Onn Kit on 13/8/18.
 //  Copyright © 2018 Kan Onn Kit. All rights reserved.
 //
+// NOTE: This is a subview where info is gotten from the
+//       previous view.
+//
 
 import UIKit
 
@@ -31,12 +34,16 @@ class ViewController_ExtraDetail: UIViewController {
         
         img_attractionImg.layer.cornerRadius = 8.0
         img_attractionImg.clipsToBounds = true
-        img_attractionImg.image = attractionImage!
+        img_attractionImg.layer.borderColor = UIColor.black.cgColor
+        img_attractionImg.layer.borderWidth = 2.0
+        img_attractionImg.image = attractionImage!  // Set the image
         
         txt_attractionDetail.layer.cornerRadius = 8.0
-        txt_attractionDetail.text = attractionDetail!.replacingOccurrences(of: "\\n", with: "\n")
+        txt_attractionDetail.layer.borderColor = UIColor.black.cgColor
+        txt_attractionDetail.layer.borderWidth = 2.0
+        txt_attractionDetail.text = attractionDetail!.replacingOccurrences(of: "\\n", with: "\n")  // Properly parse the data
         
-        label_imgCredit.text = "Image by \(imageCredits!)"
+        label_imgCredit.text = "Image by \(imageCredits!)"  // Credits for the image
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
